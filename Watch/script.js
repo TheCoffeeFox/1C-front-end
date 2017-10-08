bad270fb6c00b8c86029adf09a3efba30d4d9973
TheCoffeeFox <lkokachev@mail.ru>
2017-10-08 22:19:40 +0300

    Watch DONE!!!
    Закончил оформление часов. Цвет стрелок, толщина. Осталось только числа раставить. Можно ещё фон закрасить 	¯\_(ツ)_/¯

--- Watch/script.js
+++ Watch/script.js
@@ -1,12 +1,19 @@
 var canvas = document.getElementById("sandbox"),
     context = canvas.getContext("2d"),
-    square, circle;
+    square,l1,l2,l3;
 
 function drawWatch(){
+context.clearRect(0, 0, 300, 300);
 
-square = new Path2D();    
+
+square = new Path2D();
+l1= new Path2D(); 
+l2= new Path2D(); 
+l3= new Path2D(); 
 
 var R=300/2,d,angle,pX,pY,qX,qY;
+
+square.arc(R,R,R,0,2*Math.PI);
 
 for(d=0; d<60; ++d){
     angle = (d/60)*(2*Math.PI);
@@ -19,7 +26,7 @@ for(d=0; d<60; ++d){
     square.moveTo(pX,pY);
     square.lineTo(qX,qY);
 }
-
+    
 var date = new Date(), hours, minutes, seconds;
 
 hours = date.getHours();
@@ -37,27 +44,36 @@ secondsAngle = Math.PI/2 - secondsAngle;
 minutesAngle = Math.PI/2 - minutesAngle;
 hoursAngle = Math.PI/2 - hoursAngle;
 
-sX= Math.cos(secondsAngle)*R;
-sY= -Math.sin(secondsAngle)*R;
+sX= Math.cos(secondsAngle)*0.89*R;
+sY= -Math.sin(secondsAngle)*0.89*R;
 sX +=R; sY +=R;
-square.moveTo(sX,sY);
-square.lineTo(R,R);
+l1.moveTo(sX,sY);
+l1.lineTo(R,R);
     
 mX= Math.cos(minutesAngle)*0.75*R;
 mY= -Math.sin(minutesAngle)*0.75*R;
 mX +=R; mY +=R;
-square.moveTo(mX,mY);
-square.lineTo(R,R);
+l2.moveTo(mX,mY);
+l2.lineTo(R,R);
     
 hX= Math.cos(hoursAngle)*0.25*R;
 hY= -Math.sin(hoursAngle)*0.25*R;
 hX +=R; hY +=R;
-square.moveTo(hX,hY);
-square.lineTo(R,R);
-
-context.clearRect(0, 0, 300, 300);
-context.stroke(square);
-
+l3.moveTo(hX,hY);
+l3.lineTo(R,R);
+    
+    context.lineCap = "round";
+    context.lineWidth = 1;
+context.stroke(square); 
+    context.strokeStyle= "red";
+context.stroke(l1);
+    context.strokeStyle= "black";
+    context.lineWidth = 3;
+context.stroke(l2);
+    context.lineWidth = 7;
+context.stroke(l3);
+   
+    
 setTimeout(drawWatch, 1000);
 }
 drawWatch();

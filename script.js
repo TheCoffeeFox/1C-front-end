7757503a1107ae1f1417dc92c6c9ed97fa257c9c
TheCoffeeFox <lkokachev@mail.ru>
2017-10-08 15:15:19 +0300

    WTF Watch
    Почти часы. Доделать

--- script.js
+++ script.js
@@ -3,14 +3,60 @@ var canvas = document.getElementById("sandbox"),
     square, circle;
 
 square = new Path2D();
-square.moveTo(50, 50);
-square.lineTo(250, 50);
-square.lineTo(250, 250);
-square.lineTo(50, 250);
-square.lineTo(50, 50);
 
-context.stroke(square);
+var R=300/2,d,angle,pX,pY,qX,qY;
 
-circle = new Path2D();
-circle.arc(150, 150, 100, 0, 2 * Math.PI);
-context.fill(circle);
+for(d=0; d<60; ++d){
+    angle = (d/60)*(2*Math.PI);
+    pX= Math.cos(angle)*R;
+    pY= -Math.sin(angle)*R;
+    qX= 0.9*pX;
+    qY= 0.9*pY;
+    pX +=R; pY +=R;
+    qX +=R; qY +=R;
+    square.moveTo(pX,pY);
+    square.lineTo(qX,qY);
+}
+
+function drawWatch(){
+    context.clearRect(0,0,300,300);
+    context.stroke(square);
+    
+var date = new Date(), hours, minutes, seconds;
+
+hours = date.getHours();
+minutes = date.getMinutes();
+seconds = date.getSeconds();
+
+var secondsAngle, minutesAngle, hoursAngle;
+var sX, sY, mX, mY, hX, hY;
+
+secondsAngle = (seconds/60)*(2*Math.PI);
+minutesAngle = (minutes/60)*(2*Math.PI);
+hoursAngle = ((hours % 12)/12)*(2*Math.PI);
+
+secondsAngle = Math.PI/2 - secondsAngle;
+minutesAngle = Math.PI/2 - minutesAngle;
+hoursAngle = Math.Pi/2 - hoursAngle;
+
+sX= Math.cos(secondsAngle)*R;
+sY= -Math.sin(secondsAngle)*R;
+sX +=R; sY +=R;
+square.moveTo(sX,sY);
+square.lineTo(R,R);
+    
+mx= Math.cos(minutesAngle)*R;
+mY= -Math.sin(minutesAngle)*R;
+mX +=R; mY +=R;
+square.moveTo(mX,mY);
+square.lineTo(R,R);
+    
+hX= Math.cos(hoursAngle)*R;
+hY= -Math.sin(hoursAngle)*R;
+hX +=R; hY +=R;
+square.moveTo(hX,hY);
+square.lineTo(R,R);
+
+setTimeout(drawWatch,1000);
+}
+drawWatch();

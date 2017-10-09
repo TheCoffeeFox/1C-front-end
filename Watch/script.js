9db8af60b0131b6cbc598f47bcef04986428a580
TheCoffeeFox <lkokachev@mail.ru>
2017-10-09 21:46:27 +0300

    Clock DONE!!!!
    Закончил делать часы. (Конечно без чисел по круг, но не суть) Изменил размер часов, добавил задний фон. Добавил изменение минутной/часов стрел от секундой/минутной соответственно.

--- Watch/script.js
+++ Watch/script.js
@@ -3,19 +3,24 @@ var canvas = document.getElementById("sandbox"),
     square,l1,l2,l3;
 
 function drawWatch(){
-context.clearRect(0, 0, 300, 300);
+    context.clearRect(0, 0, 1000, 1000);
     context.lineCap = "round";
-    context.lineWidth = 1;
+    context.lineWidth = 4;
 
 square = new Path2D();
 l1= new Path2D(); 
 l2= new Path2D(); 
 l3= new Path2D(); 
 
-var R=300/2,d,angle,pX,pY,qX,qY;
+var R=1000/2,d,angle,pX,pY,qX,qY;
 
-square.arc(R,R,R,0,2*Math.PI);
-
+    //context.fillStyle = "#FFE2C4";
+    //context.beginPath();
+    //context.arc(R,R,R,0,2*Math.PI);
+    //context.closePath();
+    //context.fill();
+    square.arc(R,R,R,0,2*Math.PI);
+    
 for(d=0; d<60; ++d){
     angle = (d/60)*(2*Math.PI);
     pX= Math.cos(angle)*R;
@@ -27,7 +32,7 @@ for(d=0; d<60; ++d){
     square.moveTo(pX,pY);
     square.lineTo(qX,qY);
 }
-    
+        
 var date = new Date(), hours, minutes, seconds;
 
 hours = date.getHours();
@@ -42,8 +47,8 @@ minutesAngle = (minutes/60)*(2*Math.PI);
 hoursAngle = ((hours % 12)/12)*(2*Math.PI);
 
 secondsAngle = Math.PI/2 - secondsAngle;
-minutesAngle = Math.PI/2 - minutesAngle;
-hoursAngle = Math.PI/2 - hoursAngle;
+minutesAngle = Math.PI/2 - minutesAngle + secondsAngle/60;
+hoursAngle = Math.PI/2 - hoursAngle + minutesAngle/60;
 
 sX= Math.cos(secondsAngle)*0.89*R;
 sY= -Math.sin(secondsAngle)*0.89*R;
@@ -57,8 +62,8 @@ mX +=R; mY +=R;
 l2.moveTo(mX,mY);
 l2.lineTo(R,R);
     
-hX= Math.cos(hoursAngle)*0.25*R;
-hY= -Math.sin(hoursAngle)*0.25*R;
+hX= Math.cos(hoursAngle)*0.45*R;
+hY= -Math.sin(hoursAngle)*0.45*R;
 hX +=R; hY +=R;
 l3.moveTo(hX,hY);
 l3.lineTo(R,R);
@@ -67,12 +72,11 @@ context.stroke(square);
     context.strokeStyle= "red";
 context.stroke(l1);
     context.strokeStyle= "black";
-    context.lineWidth = 3;
-context.stroke(l2);
     context.lineWidth = 7;
+context.stroke(l2);
+    context.lineWidth = 15;
 context.stroke(l3);
    
-    
 setTimeout(drawWatch, 1000);
 }
 drawWatch();
